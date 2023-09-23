d82f783846c55dd30762628d6b74aa738839a80c
Robert Seals <robert.seals@gmail.com>
2023-09-23 16:29:57 -0400

    Initial Checkin

---
+++ src/main/java/java/sage/spring/spatial/JavaSageSpatialPostGisApplication.java
@@ -0,0 +1,13 @@
+package java.sage.spring.spatial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSageSpatialPostGisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSageSpatialPostGisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/sage/spring/spatial/JavaSageSpatialPostGisApplicationTests.java
@@ -0,0 +1,13 @@
+package java.sage.spring.spatial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSageSpatialPostGisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
